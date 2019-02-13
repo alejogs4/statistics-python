67945d836cbeb642e3fc11840bea2ba701c92679
Alejandro Garcia Serna <alejogs4@gmail.com>
2019-02-13 11:05:39 -0500

    Update intervals.py

--- intervals.py
+++ intervals.py
@@ -1,6 +1,14 @@
 from math import sqrt
+
 def p_kwon(average, s, n, z):
     standard = s / sqrt(n)
     minimun = average - z*standard
     maximun = average + z*standard
     return (minimun, maximun)
+
+def get_interval(n, n_special, z):
+    p = n_special / n
+    sp = sqrt((p * (1 - p)) /n)
+    mini = p - z * sp
+    maxi = p + z * sp
+    return (mini, maxi)
